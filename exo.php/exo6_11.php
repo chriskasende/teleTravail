6e144936ea0e20302b18a2248594b456b3de8cc1
christelle kasende <chriskasende@hotmail.com>
2021-03-12 11:48:33 +0100

    ajout d'exercices

--- exo.php/exo6_11.php
+++ exo.php/exo6_11.php
@@ -1,19 +1,23 @@
+<?php
 
- /*algorithm 6.11
-variables
-	i, j, array1Size, array2Size : integer;
-	array1(), array2(): array of integer;
-Begin
-	schtroumpf := 0;
-	for(i := 0, i < array2Size; i++){
-		for(j := 0, j < array1Size, j++){
-			schtroumpf : = schtroumpf + array1(j) * array2(i);
+
+function schtroumph()
+{
+	$arraysize1 = readline("Entrez le nombre de valeurs du tableau 1 : ");
+	for ($i = 0; $i < $arraysize1; $i++) {
+		$tab1[$i] = readline("entrez la valeur numero " . ($i + 1) . " : ");
+	}
+	$arraysize2 = readline("Entrez le nombre de valeurs du tableau 2: ");
+	for ($i = 0; $i < $arraysize2; $i++) {
+		$tab2[$i] = readline("entrez la valeur numero " . ($i + 1) . " : ");
+	}
+
+	$s = 0;
+	for ($i = 0; $i < $arraysize1; $i++) {
+		for ($j = 0; $j < $arraysize2; $j++) {
+			$s = $s + $tab1[$i] * $tab2[$j];
 		}
 	}
-	display(schtroumpf);
-End*/ 
-<?php
-function schtroumph(){
-	$s=0;
-	for ($i=0;$i<$arraySize;$i++)
+	echo $s;
 }
+schtroumph();
